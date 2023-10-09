7fd8e455154e3ff92f412f6aae5ffa51c45b8c90
Elul Asfaha <scorpiondota22@gmail.com>
2023-10-09 18:37:37 +0300

    apple airpod page clone

---
+++ src/components/Nav.tsx
@@ -0,0 +1,24 @@
+const Nav = () => {
+    return (
+        <div className='flex justify-center px-5 text-sm bg-[#E7E7E8] py-2'>
+            <div className='flex justify-between py-1 w-full max-w-[1024px]'>
+                <div>logo</div>
+                <div>Store</div>
+                <div>Mac</div>
+                <div>iPad</div>
+                <div>iPhone</div>
+                <div>Watch</div>
+                <div>Vision</div>
+                <div>AirPods</div>
+                <div>TV & Home</div>
+                <div>Entertainment</div>
+                <div>Accessirues</div>
+                <div>Support</div>
+                <div>search icon</div>
+                <div>bag icon</div>
+            </div>
+        </div>
+    );
+};
+
+export default Nav;
---
+++ src/pages/Home.tsx
@@ -0,0 +1,9 @@
+const Home = () => {
+    return (
+        <div className='flex flex-col justify-center max-w-[1520px] w-full'>
+            Home
+        </div>
+    );
+};
+
+export default Home;
