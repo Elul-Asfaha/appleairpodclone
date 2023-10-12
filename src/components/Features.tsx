66321bda9cae38c815c9a01632e36223474ea1bf
Elul Asfaha <scorpiondota22@gmail.com>
2023-10-13 00:40:34 +0300

    progress on features component

--- src/components/Features.tsx
+++ src/components/Features.tsx
@@ -1,13 +1,61 @@
 import { AiOutlineRight, AiOutlineLeft, AiOutlinePlus } from "react-icons/ai";
+import { useState } from "react";
 const Features = () => {
+    const [move, setMove] = useState(0);
+
+    const handleMove = (moveTo: string) => {
+        moveTo === "right" && move < 4 && setMove((prev) => prev + 1);
+        moveTo === "left" && move > 0 && setMove((prev) => prev - 1);
+        console.log(moveTo);
+    };
     return (
         <div className='max-w-[1520px] flex flex-col w-full overflow-x-hidden md:pt-32 items-center text-center px-5'>
             <p className='text-5xl font-bold w-[500px] text-center  md:mb-16'>
                 A magical connection to your devices.
             </p>
-            <div className='w-full'>
-                <div className='flex gap-5'>
+            <div className='w-full scrollbar-hide'>
+                <div
+                    className='flex gap-5'
+                    style={{ transform: `translateX(-${move * 370}px)` }}
+                >
                     <div className='relative min-w-[350px] bg-white min-h-[450px] rounded-xl'>
+                        1
+                        <div className='absolute inset-x-0 flex w-full items-center justify-between px-5 font-bold text-2xl bottom-5'>
+                            <p>Automatic Switching</p>
+                            <button className='bg-black text-white p-1 rounded-full'>
+                                <AiOutlinePlus />
+                            </button>
+                        </div>
+                    </div>
+                    <div className='relative min-w-[350px] bg-white min-h-[450px] rounded-xl'>
+                        2
+                        <div className='absolute inset-x-0 flex w-full items-center justify-between px-5 font-bold text-2xl bottom-5'>
+                            <p>Automatic Switching</p>
+                            <button className='bg-black text-white p-1 rounded-full'>
+                                <AiOutlinePlus />
+                            </button>
+                        </div>
+                    </div>
+                    <div className='relative min-w-[350px] bg-white min-h-[450px] rounded-xl'>
+                        3
+                        <div className='absolute inset-x-0 flex w-full items-center justify-between px-5 font-bold text-2xl bottom-5'>
+                            <p>Automatic Switching</p>
+                            <button className='bg-black text-white p-1 rounded-full'>
+                                <AiOutlinePlus />
+                            </button>
+                        </div>
+                    </div>
+                    <div className='relative min-w-[350px] bg-white min-h-[450px] rounded-xl'>
+                        4
+                        <div className='absolute inset-x-0 flex w-full items-center justify-between px-5 font-bold text-2xl bottom-5'>
+                            <p>Automatic Switching</p>
+                            <button className='bg-black text-white p-1 rounded-full'>
+                                <AiOutlinePlus />
+                            </button>
+                        </div>
+                    </div>
+                    <div className='relative min-w-[350px] bg-white min-h-[450px] rounded-xl'>
+                        5
                         <div className='absolute inset-x-0 flex w-full items-center justify-between px-5 font-bold text-2xl bottom-5'>
                             <p>Automatic Switching</p>
                             <button className='bg-black text-white p-1 rounded-full'>
@@ -18,10 +66,16 @@ const Features = () => {
                 </div>
             </div>
             <div className='flex gap-5 py-5'>
-                <button className='border-2 border-black rounded-full p-2 text-lg font-bold'>
+                <button
+                    className='border-2 border-black rounded-full p-2 text-lg font-bold'
+                    onClick={() => handleMove("left")}
+                >
                     <AiOutlineLeft />
                 </button>
-                <button className='border-2 border-black rounded-full p-2 text-lg font-bold'>
+                <button
+                    className='border-2 border-black rounded-full p-2 text-lg font-bold'
+                    onClick={() => handleMove("right")}
+                >
                     <AiOutlineRight />
                 </button>
             </div>
